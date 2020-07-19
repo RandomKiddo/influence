1d43e67f75353d6171a18d67b09da09601c32191
RandomKiddo <68043820+RandomKiddo@users.noreply.github.com>
2020-07-19 11:29:13 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from distutils.core import setup
 
+md = []
+with open('/Users/SchoolOfficial/Desktop/influence/README.md', 'r') as f:
+    for line in f:
+        md.append(str(line))
+
 setup(
   name = 'influence',         # How you named your package folder (MyLib)
   packages = ['influence'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A utility package for python containing two sub-packages that 1. extend python, and 2. add to python',   # Give a short description about your library
+  long_description = md,
   author = 'Neil',                   # Type in your name
   author_email = 'nghugare2@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/RandomKiddo/influence',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/RandomKiddo/influence/archive/v01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/RandomKiddo/influence/archive/v_04.tar.gz',    # I explain this later on
   keywords = ['PYTHON', 'EXTENDER', 'UPGRADER'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
