318b49220d6dd78a6d16133ac7d649dcb5e591a0
RandomKiddo <68043820+RandomKiddo@users.noreply.github.com>
2020-07-18 22:24:46 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 setup(
   name = 'influence',         # How you named your package folder (MyLib)
   packages = ['influence'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A utility package for python containing two sub-packages that 1. extend python, and 2. add to python',   # Give a short description about your library
+  long_description = 'See https://github.com/RandomKiddo/influence',
   author = 'Neil',                   # Type in your name
   author_email = 'nghugare2@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/RandomKiddo/influence',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/RandomKiddo/influence/archive/v01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/RandomKiddo/influence/archive/v_03.tar.gz',    # I explain this later on
   keywords = ['PYTHON', 'EXTENDER', 'UPGRADER'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
