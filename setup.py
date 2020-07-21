9b399816a6664b8f8feed5d192c8f2f9d2925728
RandomKiddo <68043820+RandomKiddo@users.noreply.github.com>
2020-07-20 23:11:26 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -31,11 +31,11 @@ setup(
   url = 'https://github.com/RandomKiddo/influence',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/RandomKiddo/influence/archive/v_06.tar.gz',    # I explain this later on
   keywords = ['PYTHON', 'EXTENDER', 'UPGRADER'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'numpy',
-          'matplotlib',
-          'wheel',
-      ],
+  #install_requires=[            # I get to this in a second
+          #'numpy',
+          #'matplotlib',
+          #'wheel',
+      #],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
@@ -44,5 +44,5 @@ setup(
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.8',
   ],
-  setup_requires=['wheel'],
+  #setup_requires=['wheel'],
 )
