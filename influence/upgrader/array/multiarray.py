fd273fd11b29d33f472fd8d5d07866f58fe62ce7
RandomKiddo <68043820+RandomKiddo@users.noreply.github.com>
2020-08-03 12:25:48 -0400

    Add files via upload

--- influence/upgrader/array/multiarray.py
+++ influence/upgrader/array/multiarray.py
@@ -1,4 +1,5 @@
 from arrays import Array
+import warnings
 
 class Array2D(Array):
 
@@ -12,15 +13,29 @@ class Array2D(Array):
             arr.append(Array(c))
         return arr
 
+    def __getitem__(self, key):
+        if key > self.capacity - 1:
+            raise IndexError
+        return self.__arr[key]
+
+    def __setitem__(self, key, item):
+        if key > self.capacity - 1:
+            raise IndexError
+        elif not isinstance(item, Array):
+            raise ValueError
+        self.__arr[key] = item
+
     def get(self, r_index, c_index):
+        warnings.warn("get is deprecated, square bracket notation preferred", DeprecationWarning)
         if r_index > self.capacity - 1 or c_index > self.__arr[0].capacity - 1:
             raise IndexError
-        return self.__arr[r_index][c_index]
+        return (self.__arr[r_index])[c_index]
 
     def set(self, r_index, c_index, item):
+        warnings.warn("set is deprecated, square bracket notation preferred", DeprecationWarning)
         if r_index > self.capacity - 1 or c_index > self.__arr[0].capacity - 1:
             raise IndexError
-        self.__arr[r_index][c_index] = item
+        self.__arr[r_index].__setitem__(c_index, item)
 
     def print(self):
         if self.capacity == 0:
@@ -43,7 +58,7 @@ class Array2D(Array):
                     return True
         return False
 
-    def find(self, item):
+    def index(self, item):
         r_index = 0
         c_index = 0
         for i in self.__arr:
@@ -54,5 +69,6 @@ class Array2D(Array):
                 c_index += 1
             r_index += 1
         return [-1]
-    
-    
+
+    def __len__(self):
+        return len(self.__arr)
